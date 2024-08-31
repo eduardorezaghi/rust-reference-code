722c5fe5586dc044bcdacbeae990cfe2961fbed8
Eduardo Rezaghi <eduardo.rezaghi@gmail.com>
2024-08-31 16:58:36 -0300

    Add multi-lang hello example

--- references/hello_world/src/main.rs
+++ references/hello_world/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
+fn greet_world() {
     println!("Hello, world!");
+    let southern_germany = "Grüß Gott!";
+    let japan = "こんにちは";
+    let china = "你好";
+    let emojiland = "Hello 👋🌍";
+
+    let regions = [southern_germany, japan, china, emojiland];
+    for region in regions.iter() {
+        println!("{}", &region);
+    }
+}
+
+fn main() {
+    greet_world();
 }
